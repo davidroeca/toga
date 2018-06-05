018efae02b4b71e2628ea397ca1ffaeaf6047c2d
davidroeca <david.roeca@gmail.com>
2018-06-05 01:46:46 -0400

    update button examples

--- examples/button/button/app.py
+++ examples/button/button/app.py
@@ -8,7 +8,7 @@ class ExampleButtonApp(toga.App):
     def startup(self):
         # Window class
         #   Main window of the application with title and size
-        self.main_window = toga.MainWindow(title=self.name, size=(200, 200))
+        self.main_window = toga.MainWindow(title=self.name, size=(500, 200))
 
         # Common style of the inner boxes
         style_inner_box = Pack(direction=ROW)
@@ -16,7 +16,7 @@ class ExampleButtonApp(toga.App):
         # Button class
         #   Simple button with label and callback function called when
         #   hit the button
-        button1 = toga.Button('Change Label', on_press=self.callbackLabel)
+        button1 = toga.Button('Change Label', on_press=self.callback_label)
 
         # Button with label and enable option
         button2 = toga.Button('Disabled', enabled=False)
@@ -26,7 +26,7 @@ class ExampleButtonApp(toga.App):
 
         # Button with label and callback function called when
         #   hit the button
-        button4 = toga.Button('Resize Window', on_press=self.callbackResize)
+        button4 = toga.Button('Resize Window', on_press=self.callback_resize)
 
         # Box class
         # Container of components
@@ -60,10 +60,29 @@ class ExampleButtonApp(toga.App):
             ]
         )
 
+        # Button with serif font
+        button8 = toga.Button(
+            'Serif Font',
+            style=Pack(font_family='serif', font_size=12.0)
+        )
+
+        button9 = toga.Button(
+            'Sans Serif Font',
+            style=Pack(font_family='sans-serif', font_size=15.0)
+        )
+
+        inner_box3 = toga.Box(
+            style=style_inner_box,
+            children=[
+                button8,
+                button9,
+            ],
+        )
+
         #  Create the outer box with 2 rows
         outer_box = toga.Box(
             style=Pack(direction=COLUMN, height=10),
-            children=[inner_box1, inner_box2]
+            children=[inner_box1, inner_box2, inner_box3]
         )
 
         # Add the content on the main window
@@ -72,12 +91,12 @@ class ExampleButtonApp(toga.App):
         # Show the main window
         self.main_window.show()
 
-    def callbackLabel(self, button):
+    def callback_label(self, button):
         # Some action when you hit the button
         #   In this case the label will change
         button.label = 'Magic!!'
 
-    def callbackResize(self, button):
+    def callback_resize(self, button):
         # Some action when you hit the button
         #   In this case the window size will change
         self.main_window.size = (100, 100)
